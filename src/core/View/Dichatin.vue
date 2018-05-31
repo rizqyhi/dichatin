585528bb689f1255b8e0d0fe5adaa25004d290be
Rizqy H <rizqy22@gmail.com>
2018-05-31 13:51:48 +0700

    Implementing when chat toggle was clicked

---
+++ src/core/View/Dichatin.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="dichatin">
+    <a href="#" class="dichatin-toggle" @click="toggleChatWindow()">Toggle Chat</a>
+    <div class="dichatin-window" v-show="isChatWindowShown">
+      <a href="#" class="dichatin-nav__home">Home</a>
+      <a href="#" class="dichatin-nav__quick-help">Help</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isChatWindowShown: false
+    }
+  },
+
+  methods: {
+    toggleChatWindow () {
+      this.isChatWindowShown = !this.isChatWindowShown
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ test/unit/specs/DisplayingChatWindow.spec.js
@@ -0,0 +1,43 @@
+import { shallowMount } from '@vue/test-utils'
+import Dichatin from '@/core/View/Dichatin'
+
+describe('Displaying chat window', () => {
+  context('When chat toggle was clicked', () => {
+    it('should execute event handler method', () => {
+      const wrapper = shallowMount(Dichatin)
+      const spy = sinon.spy()
+      wrapper.setMethods({ toggleChatWindow: spy })
+      wrapper.find('.dichatin-toggle').trigger('click')
+
+      expect(spy.calledOnce).to.be.true()
+    })
+
+    it('should change the chat window state', () => {
+      const wrapper = shallowMount(Dichatin)
+      wrapper.find('.dichatin-toggle').trigger('click')
+
+      expect(wrapper.vm.isChatWindowShown).to.be.true()
+    })
+
+    it('should see chat window rendered', () => {
+      const wrapper = shallowMount(Dichatin)
+      wrapper.find('.dichatin-toggle').trigger('click')
+
+      expect(wrapper.find('.dichatin-window').isVisible()).to.be.true()
+    })
+
+    it('should see home button rendered', () => {
+      const wrapper = shallowMount(Dichatin)
+      wrapper.find('.dichatin-toggle').trigger('click')
+
+      expect(wrapper.find('.dichatin-nav__home').isVisible()).to.be.true()
+    })
+
+    it('should see quick help button rendered', () => {
+      const wrapper = shallowMount(Dichatin)
+      wrapper.find('.dichatin-toggle').trigger('click')
+
+      expect(wrapper.find('.dichatin-nav__quick-help').isVisible()).to.be.true()
+    })
+  })
+})
