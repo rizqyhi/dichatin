bc335f117bc18a2b769a28d372d61d28a4f62fe2
Rizqy H <rizqy22@gmail.com>
2018-06-04 11:21:44 +0700

    Split switching section test to smaller context

--- test/unit/specs/SwitchingChatToHomeSection.spec.js
+++ test/unit/specs/SwitchingChatToHomeSection.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import AuthInformationProvider from '@/core/AuthInformationProvider'
 import Dichatin from '@/core/View/Dichatin'
 
-describe('Switching Dichatin active section', () => {
+describe('Switching Dichatin section from chat to home', () => {
   let wrapper
 
   beforeEach(() => {
@@ -12,73 +12,39 @@ describe('Switching Dichatin active section', () => {
     const authInformationProvider = new AuthInformationProvider(chatProvider)
     wrapper = shallowMount(Dichatin, {
       propsData: { authInformationProvider },
-      data: { isChatWindowShown: true }
-    })
-  })
-
-  context('When chat button was clicked', () => {
-    it('should see active section state was changed', () => {
-      wrapper.find('.dichatin-nav__chat').trigger('click')
-
-      expect(wrapper.vm.activeSection).to.equal('chat')
-    })
-
-    it('should see active chat button', () => {
-      wrapper.find('.dichatin-nav__chat').trigger('click')
-
-      expect(wrapper.find('.dichatin-nav__chat').classes()).to.contain('dichatin-nav--active')
-    })
-
-    it('should not see other buttons as active', () => {
-      wrapper.find('.dichatin-nav__chat').trigger('click')
-
-      expect(wrapper.find('.dichatin-nav:not(.dichatin-nav__chat)').classes()).to.not.contain('dichatin-nav--active')
-    })
-
-    it('should see chat section', () => {
-      wrapper.find('.dichatin-nav__chat').trigger('click')
-
-      expect(wrapper.find('.dichatin-section__chat').isVisible()).to.be.true()
-    })
-
-    it('should not see other sections', () => {
-      wrapper.find('.dichatin-nav__chat').trigger('click')
-
-      expect(wrapper.find('.dichatin-section:not(.dichatin-section__chat)').isVisible()).to.be.false()
+      data: {
+        isChatWindowShown: true,
+        activeSection: 'chat'
+      }
     })
   })
 
   context('When home button was clicked', () => {
     it('should see active section state was changed', () => {
-      wrapper.find('.dichatin-nav__chat').trigger('click')
       wrapper.find('.dichatin-nav__home').trigger('click')
 
       expect(wrapper.vm.activeSection).to.equal('home')
     })
 
     it('should see active chat button', () => {
-      wrapper.find('.dichatin-nav__chat').trigger('click')
       wrapper.find('.dichatin-nav__home').trigger('click')
 
       expect(wrapper.find('.dichatin-nav__home').classes()).to.contain('dichatin-nav--active')
     })
 
     it('should not see other buttons as active', () => {
-      wrapper.find('.dichatin-nav__chat').trigger('click')
       wrapper.find('.dichatin-nav__home').trigger('click')
 
       expect(wrapper.find('.dichatin-nav:not(.dichatin-nav__home)').classes()).to.not.contain('dichatin-nav--active')
     })
 
     it('should see home section', () => {
-      wrapper.find('.dichatin-nav__chat').trigger('click')
       wrapper.find('.dichatin-nav__home').trigger('click')
 
       expect(wrapper.find('.dichatin-section__home').isVisible()).to.be.true()
     })
 
     it('should not see other sections', () => {
-      wrapper.find('.dichatin-nav__chat').trigger('click')
       wrapper.find('.dichatin-nav__home').trigger('click')
 
       expect(wrapper.find('.dichatin-section:not(.dichatin-section__home)').isVisible()).to.be.false()
---
+++ test/unit/specs/SwitchingHomeToChatSection.spec.js
@@ -0,0 +1,50 @@
+import { shallowMount } from '@vue/test-utils'
+import AuthInformationProvider from '@/core/AuthInformationProvider'
+import Dichatin from '@/core/View/Dichatin'
+
+describe('Switching Dichatin active section from home to chat', () => {
+  let wrapper
+
+  beforeEach(() => {
+    const chatProvider = {
+      isLogin: () => true
+    }
+    const authInformationProvider = new AuthInformationProvider(chatProvider)
+    wrapper = shallowMount(Dichatin, {
+      propsData: { authInformationProvider },
+      data: { isChatWindowShown: true }
+    })
+  })
+
+  context('When chat button was clicked', () => {
+    it('should see active section state was changed', () => {
+      wrapper.find('.dichatin-nav__chat').trigger('click')
+
+      expect(wrapper.vm.activeSection).to.equal('chat')
+    })
+
+    it('should see active chat button', () => {
+      wrapper.find('.dichatin-nav__chat').trigger('click')
+
+      expect(wrapper.find('.dichatin-nav__chat').classes()).to.contain('dichatin-nav--active')
+    })
+
+    it('should not see other buttons as active', () => {
+      wrapper.find('.dichatin-nav__chat').trigger('click')
+
+      expect(wrapper.find('.dichatin-nav:not(.dichatin-nav__chat)').classes()).to.not.contain('dichatin-nav--active')
+    })
+
+    it('should see chat section', () => {
+      wrapper.find('.dichatin-nav__chat').trigger('click')
+
+      expect(wrapper.find('.dichatin-section__chat').isVisible()).to.be.true()
+    })
+
+    it('should not see other sections', () => {
+      wrapper.find('.dichatin-nav__chat').trigger('click')
+
+      expect(wrapper.find('.dichatin-section:not(.dichatin-section__chat)').isVisible()).to.be.false()
+    })
+  })
+})
