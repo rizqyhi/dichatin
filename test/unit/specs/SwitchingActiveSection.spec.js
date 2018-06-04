4c59bfae3b22882d02172de626cccae271c2da34
Rizqy H <rizqy22@gmail.com>
2018-06-04 09:53:58 +0700

    New test: Switching Dichatin active section

---
+++ test/unit/specs/SwitchingActiveSection.spec.js
@@ -0,0 +1,13 @@
+describe('Switching Dichatin active section', () => {
+  context('When chat button was clicked', () => {
+    it('should see active section state was changed')
+    it('should see chat section')
+    it('should not see other sections')
+  })
+
+  context('When home button was clicked', () => {
+    it('should see active section state was changed')
+    it('should see home section')
+    it('should not see other sections')
+  })
+})
