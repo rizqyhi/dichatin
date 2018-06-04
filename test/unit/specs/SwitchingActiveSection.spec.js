8d189b445c584cf4f9ea6b8c4f771b596657cc36
Rizqy H <rizqy22@gmail.com>
2018-06-04 09:57:53 +0700

    Test should check correct state after section changed

--- test/unit/specs/SwitchingActiveSection.spec.js
+++ test/unit/specs/SwitchingActiveSection.spec.js
@@ -1,6 +1,28 @@
+import { shallowMount } from '@vue/test-utils'
+import AuthInformationProvider from '@/core/AuthInformationProvider'
+import Dichatin from '@/core/View/Dichatin'
+
 describe('Switching Dichatin active section', () => {
+  let wrapper
+
+  beforeEach(() => {
+    const chatProvider = {
+      isLogin: () => true
+    }
+    const authInformationProvider = new AuthInformationProvider(chatProvider)
+    wrapper = shallowMount(Dichatin, {
+      propsData: { authInformationProvider },
+      data: { isChatWindowShown: true }
+    })
+  })
+
   context('When chat button was clicked', () => {
-    it('should see active section state was changed')
+    it('should see active section state was changed', () => {
+      wrapper.find('.dichatin-nav__chat').trigger('click')
+
+      expect(wrapper.vm.activeSection).to.equal('chat')
+    })
+
     it('should see chat section')
     it('should not see other sections')
   })
