9e9ebcfc39349fd659bd685624848970994c65d2
Rizqy H <rizqy22@gmail.com>
2018-06-05 10:31:29 +0700

    Add test when switching quick help to home section

---
+++ test/unit/specs/SwitchingQuickHelpToHomeSection.spec.js
@@ -0,0 +1,55 @@
+import { shallowMount } from '@vue/test-utils'
+import AuthInformationProvider from '@/core/AuthInformationProvider'
+import PersistedSectionManager from '@/core/PersistedSectionManager'
+import Dichatin from '@/core/View/Dichatin'
+
+describe('Switching Dichatin active section from quick help to home', () => {
+  let wrapper
+
+  beforeEach(() => {
+    const chatProvider = {
+      isLogin: () => true
+    }
+    const authInformationProvider = new AuthInformationProvider(chatProvider)
+    const persistedSectionManager = new PersistedSectionManager()
+    wrapper = shallowMount(Dichatin, {
+      propsData: { authInformationProvider, persistedSectionManager },
+      data: {
+        isChatWindowShown: true,
+        activeSection: 'quick-help'
+      }
+    })
+  })
+
+  context('When home button was clicked', () => {
+    it('should see active section state was changed', () => {
+      wrapper.find('.dichatin-nav__home').trigger('click')
+
+      expect(wrapper.vm.activeSection).to.equal('home')
+    })
+
+    it('should see active home button', () => {
+      wrapper.find('.dichatin-nav__home').trigger('click')
+
+      expect(wrapper.find('.dichatin-nav__home').classes()).to.contain('dichatin-nav--active')
+    })
+
+    it('should not see other buttons as active', () => {
+      wrapper.find('.dichatin-nav__home').trigger('click')
+
+      expect(wrapper.find('.dichatin-nav:not(.dichatin-nav__home)').classes()).to.not.contain('dichatin-nav--active')
+    })
+
+    it('should see home section', () => {
+      wrapper.find('.dichatin-nav__home').trigger('click')
+
+      expect(wrapper.find('.dichatin-section__home').isVisible()).to.be.true()
+    })
+
+    it('should not see other sections', () => {
+      wrapper.find('.dichatin-nav__home').trigger('click')
+
+      expect(wrapper.find('.dichatin-section:not(.dichatin-section__home)').isVisible()).to.be.false()
+    })
+  })
+})
