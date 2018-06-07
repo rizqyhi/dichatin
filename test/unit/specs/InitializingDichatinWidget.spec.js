70436ba993b526bb75e6937eab500b63974e05b2
Rizqy H <rizqy22@gmail.com>
2018-06-07 09:33:17 +0700

    New spec for initializing dichatin chat testing

---
+++ test/unit/specs/InitializingDichatinWidget.spec.js
@@ -0,0 +1,12 @@
+describe('Initializing Dichatin widget', () => {
+  context('When Dichatin was called to be rendered', () => {
+    it('should returned Vue instance')
+    it('should called render function')
+    it('should be initialized with required props')
+  })
+
+  context('When Dichatin was called to be rendered without required props', () => {
+    it('should throw an exception')
+    it('should not rendered the widget')
+  })
+})
