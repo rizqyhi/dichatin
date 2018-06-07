a7f3f454b95d2af7894873810a82205f8a3227c5
Rizqy H <rizqy22@gmail.com>
2018-06-07 09:55:00 +0700

    Initializing bare Vue for dichatin widget

--- test/unit/specs/InitializingDichatinWidget.spec.js
+++ test/unit/specs/InitializingDichatinWidget.spec.js
@@ -1,6 +1,13 @@
+import Vue from 'vue'
+import DichatinWidget from '@/core/DichatinWidget'
+
 describe('Initializing Dichatin widget', () => {
   context('When Dichatin was called to be rendered', () => {
-    it('should returned Vue instance')
+    it('should returned Vue instance', () => {
+      const widget = DichatinWidget.render()
+
+      expect(widget).to.be.an.instanceOf(Vue)
+    })
     it('should be initialized with required props')
   })
 
